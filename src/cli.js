5e2558444fad7208f064cd0c521b72f6adbfc32c
Rovshan Azimov <azi.rovshan>
2021-02-23 13:52:53 +0300

    add eslint, step 4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
+
 const hello = () => {
   const name = readlineSync.question('May I have your name? ');
   return `Hello, ${name}`;
 };
-
+// eslint-disable-next-line import/prefer-default-export
 export { hello };
