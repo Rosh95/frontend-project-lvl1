6eb6c2a91da66b7e87cf9e13099563fdb9698a7c
Rosh95 <63522668+Rosh95@users.noreply.github.com>
2021-03-09 00:40:22 +0300

    fix2 brain-calc

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -22,7 +22,6 @@ const getCorrectResult = () => {
   const num2 = getRandomNumber(1, 30);
   const randomOperation = operations[getRandomNumber(0, operations.length)];
   const question = `${num1}${randomOperation}${num2}`;
-  const question = `${num1}${randomOperation}${num2}`;
   const correctExpression = getExpressionResult(randomOperation, num1, num2);
   return [question, correctAnswer];
 };
