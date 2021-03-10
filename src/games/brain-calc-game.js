0d4e346be5e333889ad5a3795f6cd9ee7c052df7
Rovshan Azimov <azi.rovshan>
2021-03-10 12:14:11 +0300

    fix brain-calc

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -18,8 +18,8 @@ const getExpressionResult = (operation, num1, num2) => {
 
 const getCorrectResult = () => {
   const operations = ['+', '-', '*'];
-  const num1 = getRandomNumber(-10, 30);
-  const num2 = getRandomNumber(-10, 30);
+  const num1 = getRandomNumber(1, 30);
+  const num2 = getRandomNumber(1, 30);
   const randomOperation = operations[getRandomNumber(0, operations.length)];
   const question = `${num1}${randomOperation}${num2}`;
   const correctAnswer = String(getExpressionResult(randomOperation, num1, num2));
